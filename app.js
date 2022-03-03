9e7c2e0020e9c9ea455c3ddd9b9932631a69867c
Natalia <onataliadelina@gmail.com>
2022-03-03 19:00:19 +0100

    post and get book

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const bookRoutes = require('./routes/bookRouts');
 const cookieParser = require('cookie-parser');
 const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 const dotenv = require("dotenv");
@@ -13,6 +14,14 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next()
+});
 
 // view engine
 app.set('view engine', 'ejs');
@@ -34,5 +43,7 @@ app.listen(port, () => {
 // routes
 app.get('*', checkUser);
 app.get('/', (req, res) => res.render('home'));
-app.get('/books', requireAuth, (req, res) => res.render('books'));
+// app.get('/books', requireAuth, getBooks, (req, res) => res.render('books'));
+app.get('/add-book', requireAuth, (req, res) => res.render('addBook'));
 app.use(authRoutes);
+app.use(bookRoutes);
