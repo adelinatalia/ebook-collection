3d3ea77296ebd7e899f4803216e12bbc6c1fe6b0
Natalia <onataliadelina@gmail.com>
2022-03-04 09:07:53 +0100

    little changes

--- routes/bookRouts.js
+++ routes/bookRouts.js
@@ -6,9 +6,9 @@ const uploadBook = require("../middleware/uploadBook");
 const router = Router();
 
 router.get('/books', requireAuth, bookController.getBooks);
-// router.get('/:id', bookController.getBook);
+// router.get('/:id', requireAuth, bookController.getBook);
 router.post('/add-book', uploadBook, bookController.postBook);
-// router.put('/:id', bookController.putBook);
-// router.delete("/:id/:filename", auth, bookController.deleteBook)
+// router.put('/:id', requireAuth, bookController.putBook);
+// router.delete("/:id/:filename", requireAuth, bookController.deleteBook)
 
 module.exports = router;
